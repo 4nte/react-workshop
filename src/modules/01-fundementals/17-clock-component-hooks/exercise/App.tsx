e82dd1034f57f0e888785481d7d00e382a079bf3
Ante Gulin <8070595+4nte@users.noreply.github.com>
2021-02-02 09:41:51 +0100

    Fix unused import

--- src/modules/01-fundementals/17-clock-component-hooks/exercise/App.tsx
+++ src/modules/01-fundementals/17-clock-component-hooks/exercise/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import Clock from "../solution/Clock";
 /* Import your component here */
 
 function App() {
   return <div>
     {/*render your component here*/}
-    <Clock/>
   </div>
 }
 
--- src/modules/01-fundementals/18-reaction-time-game/exercise/App.tsx
+++ src/modules/01-fundementals/18-reaction-time-game/exercise/App.tsx
@@ -1,11 +1,9 @@
 import React, {useEffect, useState} from 'react';
-import Game from "../solution/Game";
 /* Import your component here */
 
 function App() {
 
   return <div>
-      <Game/>
     {/*render your component here*/}
   </div>
 }
